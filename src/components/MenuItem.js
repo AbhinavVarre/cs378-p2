06ca5633026182aea19b0a50dac7c36296eb84e2
AbhinavVarre <37716743+AbhinavVarre@users.noreply.github.com>
2024-02-09 16:37:53 -0600

    remove initial backslash

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -11,7 +11,7 @@ const MenuItem = ({ title, description, imageName, price }) => {
       <li className="list-group-item">
         <div className="itemRow">
           <div className="menuImageContainer">
-            <img src={ `/images/${imageName}`}
+            <img src={ `images/${imageName}`}
             alt={imageName}
             className="menuImage"
             ></img>
